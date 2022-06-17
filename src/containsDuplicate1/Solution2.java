32b30b58eee29b6e34deff5e14cc1c3ed184709d
parasharparth <parthparashar6@gmail.com>
2022-06-17 03:23:18 -0700

    containsDuplicate-1 Solutions

--- src/containsDuplicate1/Solution2.java
+++ src/containsDuplicate1/Solution2.java
@@ -1,5 +1,8 @@
 package containsDuplicate1;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Solution2 {
 
     /****************************************************************************************
@@ -10,6 +13,21 @@ public class Solution2 {
      ****************************************************************************************/
     public static void main(String[] args)
     {
+        boolean result = Solution2.containsDuplicate(new Random().ints(100, 0 , 1000).toArray());
+        System.out.println(result);
+    }
 
+    public static boolean containsDuplicate(int[] nums)
+    {
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length-1; ++i)
+        {
+            if(nums[i] == nums[i+1])
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/containsDuplicate1/Solution3.java
+++ src/containsDuplicate1/Solution3.java
@@ -1,4 +1,32 @@
 package containsDuplicate1;
 
+import java.util.HashSet;
+import java.util.Random;
+
+/*****************************************************************************************************
+ * The third way is to use dynamic data structures which supports fast search and insert operations
+ * We will be using a hashset for this operation.
+ * This produces an almost linear runtime of O(1).
+ *****************************************************************************************************/
 public class Solution3 {
+
+    public static void main(String args[])
+    {
+        boolean result = Solution3.containsDuplicate(new Random().ints(100,0,100000).toArray());
+        System.out.println(result);
+    }
+
+    public static boolean containsDuplicate(int[] nums)
+    {
+        //convert the array to hashset by a single step operation of defining a new hashset
+        HashSet<Integer> set = new HashSet<>(nums.length);
+        for(int x:nums)
+        {
+            if(set.contains(x))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
